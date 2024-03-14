88f11ab9e854c0fc9ea60799f7a46bc467a881da
FelipeMVB1993 <136653946+FelipeMVB1993@users.noreply.github.com>
2024-03-13 23:16:24 -0300

    fix: desenvolvimento da tela de login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,73 @@
+// import React, { useState } from 'react';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import './App.css';
+// import Login from './COMPONENTES/LOGIN/Login';
+// import ComponenteMenu from "./COMPONENTES/MENU/ComponenteMenu";
+// import ComponenteTabela from './COMPONENTES/FELIPE/ComponenteTabela';
+// import DadosDisciplina from './COMPONENTES/FELIPE/DadosDisciplina';
+// import DadosAgenda from './COMPONENTES/FELIPE/DadosAgenda';
+
+
+// function App() {
+//   const [isMenuExpanded, setMenuExpanded] = useState(false);
+
+//   return (
+//     <BrowserRouter>
+//       <Login></Login>
+//       <ComponenteMenu isMenuExpanded={isMenuExpanded} setMenuExpanded={setMenuExpanded} />
+
+//       <Routes>
+//         <Route path='/cadastroAlunos' element={<ComponenteTabela isMenuExpanded={isMenuExpanded} ></ComponenteTabela>} />
+//       </Routes>
+
+//       <Routes>
+//         <Route path="/cadastroDisciplina" element={<DadosDisciplina isMenuExpanded={isMenuExpanded}/>}/>
+//       </Routes>
+
+//       <Routes>
+//         <Route path="/cadastroAgenda" element={<DadosAgenda isMenuExpanded={isMenuExpanded}/>}/>
+//       </Routes>
+
+//     </BrowserRouter>  
+//   );
+// }
+
+// export default App;
+
+
 import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import ComponenteMenu from "./COMPONENTES/MENU/ComponenteMenu";
-import ComponenteTabela from './COMPONENTES/FELIPE/ComponenteTabela'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
+import Login from './COMPONENTES/LOGIN/Login';
+import ComponenteMenu from "./COMPONENTES/MENU/ComponenteMenu";
+import ComponenteTabela from './COMPONENTES/FELIPE/ComponenteTabela';
 import DadosDisciplina from './COMPONENTES/FELIPE/DadosDisciplina';
-import DadosAgenda from './COMPONENTES/FELIPE/DadosAgenda'
-
+import DadosAgenda from './COMPONENTES/FELIPE/DadosAgenda';
 
 function App() {
+  const [isLoggedIn, setLoggedIn] = useState(false);
   const [isMenuExpanded, setMenuExpanded] = useState(false);
+
+  const handleLogin = () => {
+    // Implement your login logic here
+    setLoggedIn(true);
+  };
 
   return (
     <BrowserRouter>
+      {!isLoggedIn ? (
+        <Login path='/cadastro' onLogin={handleLogin} />
+      ) : (
+        <>
+          <ComponenteMenu path='/cadastros' isMenuExpanded={isMenuExpanded} setMenuExpanded={setMenuExpanded} />
 
-      <ComponenteMenu isMenuExpanded={isMenuExpanded} setMenuExpanded={setMenuExpanded} />
-
-      <Routes>
-        <Route path='/cadastroAlunos' element={<ComponenteTabela isMenuExpanded={isMenuExpanded} ></ComponenteTabela>} />
-      </Routes>
-
-      <Routes>
-        <Route path="/cadastroDisciplina" element={<DadosDisciplina isMenuExpanded={isMenuExpanded}/>}/>
-      </Routes>
-
-      <Routes>
-        <Route path="/cadastroAgenda" element={<DadosAgenda isMenuExpanded={isMenuExpanded}/>}/>
-      </Routes>
-
+          <Routes>
+            <Route path='/cadastroAlunos' element={<ComponenteTabela isMenuExpanded={isMenuExpanded} />} />
+            <Route path="/cadastroDisciplina" element={<DadosDisciplina isMenuExpanded={isMenuExpanded} />} />
+            <Route path="/cadastroAgenda" element={<DadosAgenda isMenuExpanded={isMenuExpanded} />} />
+          </Routes>
+        </>
+      )}
     </BrowserRouter>  
   );
 }
